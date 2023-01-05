6e3ee504755b4d67be4c89d8f7bc829a7fa07638
Javier Alonso <jackarrillo.1@gmail.com>
2023-01-05 14:26:08 -0500

    add instance3

--- modules/http_server/main.tf
+++ modules/http_server/main.tf
@@ -59,7 +59,7 @@ resource "google_compute_instance" "http_server_ubuntu" {
   }
 
   network_interface {
-    subnetwork = "${var.env}-subnet-02"
+    subnetwork = "${var.subnet} {subnet-02}"
    
 
     access_config {
